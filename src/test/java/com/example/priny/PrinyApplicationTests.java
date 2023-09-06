7195d60d5affea9f6e81da95987954fcedce77d7
김하나 <dd1246gksk@gmail.com>
2023-09-07 00:33:42 +0900

    프로젝트 초기 설정

---
+++ src/test/java/com/example/priny/PrinyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.priny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrinyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
